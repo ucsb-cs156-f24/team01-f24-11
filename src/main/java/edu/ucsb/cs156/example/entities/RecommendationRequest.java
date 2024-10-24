66c377bb4a9c8bdbbf6e22543fe5b623ee5d9fa2
Brian Cheng <brian_cheng@ucsb.edu>
2024-10-23 17:25:42 -0700

    Added Both Endpoints

--- src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/example/entities/RecommendationRequest.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "RecommendationRequest")
+@Entity(name = "recommendationrequest")
 public class RecommendationRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
