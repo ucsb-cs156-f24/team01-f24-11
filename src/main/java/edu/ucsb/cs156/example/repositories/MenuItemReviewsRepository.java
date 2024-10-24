5037720d5daf2c73fbecb06cb67ff6bed3e07f47
John Lins <johnpaullins@gmail.com>
2024-10-24 00:06:23 -0700

    Added tests

--- src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewsRepository.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDate;
+import edu.ucsb.cs156.example.entities.MenuItemReviews;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface MenuItemReviewsRepository extends CrudRepository<UCSBDate, Long> {
+public interface MenuItemReviewsRepository extends CrudRepository<MenuItemReviews, Long> {
 
 }
